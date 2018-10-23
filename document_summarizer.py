73fdcde2554864ae3d7bd2a71e8df0fcbd65e4e1
nainys <sharmanainy1711@gmail.com>
2018-10-24 00:37:16 +0530

    Create summary in Summaries folder

--- document_summarizer.py
+++ document_summarizer.py
@@ -7,9 +7,14 @@ from gensim.summarization.summarizer import summarize
 data_dir = './Datasets/cmplg-xml'
 files = os.listdir(data_dir)
 
+summary_directory = os.path.join(data_dir,"Summaries")
+# Summaries Directory
+if not os.path.exists(summary_directory):
+    os.makedirs(summary_directory)
+
 # Create corresponding summary files
 for file in files:
-	f = open(os.path.join(data_dir, file[:-3]+"txt"),"w")
+	f = open(os.path.join(summary_directory, file[:-3]+"txt"),"w")
 	text = ""
 
 	tree = et.parse(os.path.join(data_dir, file))
